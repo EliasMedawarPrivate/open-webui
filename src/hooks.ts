48aeea415f3dafa93e4098f9c8dbe5512c51b934
Elias Medawar <elias.medawar@gmail.com>
2025-05-10 11:58:22 +0200

    Adding a global Hock on Frontend to inform Backend about Type of conversation

---
+++ src/hooks.ts
@@ -0,0 +1,20 @@
+import { get } from 'svelte/store';
+import {  temporaryChatEnabled } from '$lib/stores';
+
+if (typeof window !== 'undefined') {
+  debugger;
+  const originalFetch = window.fetch;
+  window.fetch = (input, init = {}) => {
+    const raw = get(temporaryChatEnabled);
+    if (typeof raw === 'boolean') {
+      init.headers = {
+        ...(init.headers || {}),
+        'X-TemporaryChatEnabled': raw.toString()
+      };
+      return originalFetch(input, init);
+    }else{
+          return originalFetch(input, init);
+    }
+    
+  };
+}
